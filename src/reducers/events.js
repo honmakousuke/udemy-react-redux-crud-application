e7b8f8b80154aae561a7b417b693750c672af93f
honmakousuke <1644675+honmakousuke@users.noreply.github.com>
2022-01-03 20:05:54 +0900

    inplement event new component

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import { READ_EVENTS } from '../actions'
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENTS:
